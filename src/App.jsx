608450b6f110cc7ca4cccf1cb7096a9e730da57a
Tanat Arora <tanat.navin@gmail.com>
2025-03-20 01:35:17 +0700

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import { BrowserRouter } from "react-router-dom"
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, 
 Works, StarsCanvas, Footer} from './components'
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
@@ -22,6 +23,7 @@ const App = () => {
         </div>
         <Footer />
       </div>
+      <Analytics />
     </BrowserRouter>
   )
 }
